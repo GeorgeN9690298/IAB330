c3d2a6272f3868e1b7ae6079226d13a1223b8c22
Abby Gyte <abby13.gyte@gmail.com>
2022-10-17 10:23:02 +1000

    Initial creation of datascreen

---
+++ assessment2reactapp/src/Datascreen.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+import { Bar } from "react-chartjs-2";
+
+export default function Data() {
+    // 
+    const [roomData, setRoomData] = useState([]);
+
+
+    // Data that is displayed
+    return(
+        <div className="container">
+            <h1>Data Goes Here</h1>
+        </div>
+    );
+}
